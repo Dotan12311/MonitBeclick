fe83e00b90c16c4f41fd74c880d1009e2c90c6da
Dotan12311 <dotanvaknin1@gmail.com>
2023-10-24 21:49:09 +0300

    TESTTT

--- fixCities.js
+++ fixCities.js
@@ -1,24 +1,37 @@
 const fs = require('fs');
 
 // Read the contents of your cities.txt file
-const rawData = fs.readFileSync('./cities.txt', 'utf8');
+fs.readFile('./cities.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading cities.txt:', err);
+    return;
+  }
 
-// Function to parse and fix the data
-function fixCityData(data) {
-  // Split the data into lines
-  const lines = data.trim().split('\n');
-  const fixedData = [];
+  // Function to parse and fix the data
+  function fixCityData(data) {
+    // Split the data into lines
+    const lines = data.trim().split('\n');
+    const fixedData = [];
 
-  lines.forEach((line) => {
-    // Replace single quotes around key-value pairs
-    const fixedLine = line.replace(/'([^']+)':/g, (_, match) => `"${match}":`);
-    fixedData.push(fixedLine);
+    lines.forEach((line) => {
+      // Replace single quotes around key-value pairs
+      const fixedLine = line.replace(/'([^']+)':/g, (_, match) => `"${match}":`);
+      const wrappedLine = `{${fixedLine}}`; // Wrap each line in curly braces
+      fixedData.push(wrappedLine);
+    });
+
+    return `[${fixedData.join(',')}]`; // Create a valid JSON array
+  }
+
+  // Fix the data
+  const fixedData = fixCityData(data);
+
+  // Save the fixed data to a new file called fixedCities.json
+  fs.writeFile('./fixedCities.json', fixedData, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing fixedCities.json:', err);
+    } else {
+      console.log('Fixed data saved to fixedCities.json1');
+    }
   });
-
-  return fixedData.join('\n');
-}
-
-// Fix and save the data
-const fixedData = fixCityData(rawData);
-fs.writeFileSync('./fixedCities.txt', fixedData, 'utf8');
-console.log('Fixed data saved to fixedCities.txt');
+});
